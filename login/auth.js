ec5323d8417aaa285bbbb378ce67fd4f6dc6bd37
FuadMuslim1 <fuadmuslim4@gmail.com>
2025-12-24 18:34:06 +0700

    upload

---
+++ login/auth.js
@@ -0,0 +1,14 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDc24eHGwIFcltfsYOwuhig8whHQuhfJ7U",
+  authDomain: "learning-english-geuwat-d7555.firebaseapp.com",
+  projectId: "learning-english-geuwat-d7555",
+  storageBucket: "learning-english-geuwat-d7555.firebasestorage.app",
+  messagingSenderId: "110497450552",
+  appId: "1:110497450552:web:1de6e8e96d77318988b4cd",
+  measurementId: "G-SD5W12S7ZZ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
